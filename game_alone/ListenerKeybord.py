d3bac7ec9915c09967e588ee0c7e923bb22cdacc
NasWin11 <1270071138@qq.com>
2024-12-28 00:30:09 +0800

    game

--- game_alone/ListenerKeybord.py
+++ game_alone/ListenerKeybord.py
@@ -3,22 +3,23 @@ from pynput.keyboard import Listener, Key
 
 class ListenerKeybord:
     def __init__(self, config):
-        self.config = config
+        self.config = config.instance
         self.call()
+
 
     def release(self, key):
         if key == Key.shift:
-            self.config.instance.pause()
+            self.config.pause()
 
     def press(self, key):
         if key == Key.shift:
-            self.config.instance.start()
+            self.config.start()
         elif key == Key.end:
-            self.config.instance.destroy()
+            self.config.destroy()
         elif key == Key.f2:
-            self.config.instance.setRed()
+            self.config.setRed()
         elif key == Key.f1:
-            self.config.instance.setBlue()
+            self.config.setBlue()
 
     def call(self):
         with Listener(on_release=self.release, on_press=self.press) as k:
--- game_alone/SeeScreen.py
+++ game_alone/SeeScreen.py
@@ -10,16 +10,16 @@ from game_alone.YoloHead import YoloHead
 
 class SeeScreen:
     def __init__(self, config):
-        self.config = config
-        self.width = self.config.instance.width
-        self.height = self.config.instance.height
-        self.fps = self.config.instance.fps
+        self.config = config.instance
+        self.width = self.config.width
+        self.height = self.config.height
+        self.fps = self.config.fps
         self.screen_center = None
         self.overlay = ScreenOverlay(width=self.width, height=self.height)
         if not self.screen_center:
             self.get_screen_center()
         self.yolo = YoloHead(self.config.model_path, (self.width, self.height), self.config)
-        self.mouse = MouseUtils(self.config.instance.ads)
+        self.mouse = MouseUtils(self.config.ads)
         self.start_monitoring()
 
     def get_screen_center(self):
@@ -46,12 +46,11 @@ class SeeScreen:
     def start_monitoring(self):
         """实时监视屏幕中央区域"""
         delay = 1 / self.fps
-        while not self.config.instance.isDes:
+        while not self.config.isDes:
             start_time = time.time()
             screenshot = self.capture_center_area()
             frame = cv2.cvtColor(np.array(screenshot), cv2.COLOR_BGR2RGB)
-            print(self.config.instance.isStarted)
-            if self.config.instance.isStarted:
+            if self.config.isStarted:
                 pre = self.yolo.call(frame)
                 self.overlay.start(pre["x"], pre["y"])
                 if pre["shoot"]:

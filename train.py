36b63fa13eb79deecdb02880fef60482067518c9
NasWin11 <1270071138@qq.com>
2024-12-27 20:47:02 +0800

    game

--- train.py
+++ train.py
@@ -13,7 +13,8 @@ Models:     https://github.com/ultralytics/yolov5/tree/master/models
 Datasets:   https://github.com/ultralytics/yolov5/tree/master/data
 Tutorial:   https://docs.ultralytics.com/yolov5/tutorials/train_custom_data
 """
-
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 import argparse
 import math
 import os

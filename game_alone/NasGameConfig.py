313caec533b93f88fae0460db278acee7f1409ae
NasWin11 <1270071138@qq.com>
2024-12-27 23:30:48 +0800

    game

--- game_alone/NasGameConfig.py
+++ game_alone/NasGameConfig.py
@@ -2,7 +2,7 @@ class NasGameConfig:
     def __init__(self, width=640, height=640, fps=30):
         self.isStarted = False
         self.isDes = False
-        self.model_path = "runs/train/last.pt"
+        self.model_path = "csgo/best.pt"
         self.isRed = True
         self.ads = 0.95
         self.width = width
@@ -10,17 +10,22 @@ class NasGameConfig:
         self.fps = fps
 
     def start(self):
+        print("NasGameConfig start")
         self.isStarted = True
 
     def pause(self):
+        print("NasGameConfig pause")
         self.isStarted = False
 
     def destroy(self):
+        print("NasGameConfig destroy")
         self.isStarted = False
         self.isDes = True
 
     def setRed(self):
+        print("NasGameConfig setRed")
         self.isRed = True
 
     def setBlue(self):
+        print("NasGameConfig setBlue")
         self.isRed = False

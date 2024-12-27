46043c320b7963e1c2fdbcd12c220d61ad8356fd
NasWin11 <1270071138@qq.com>
2024-12-28 00:12:01 +0800

    game

--- game_alone/ListenerKeybord.py
+++ game_alone/ListenerKeybord.py
@@ -3,22 +3,23 @@ from pynput.keyboard import Listener, Key
 
 class ListenerKeybord:
     def __init__(self, config):
-        self.config = config
+        self.config = config.instance
         self.call()
 
     def release(self, key):
         if key == Key.shift:
-            self.config .pause()
+            self.config.pause()
 
     def press(self, key):
         if key == Key.shift:
-            self.config .start()
+            self.config.start()
         elif key == Key.end:
-            self.config .destroy()
+            self.config.destroy()
         elif key == Key.f2:
-            self.config .setRed()
+            self.config.setRed()
         elif key == Key.f1:
-            self.config .setBlue()
+            self.config.setBlue()
+
     def call(self):
         with Listener(on_release=self.release, on_press=self.press) as k:
             k.join()

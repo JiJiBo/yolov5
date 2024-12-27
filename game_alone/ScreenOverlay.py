29076c927e072170f33098f2feae94b21213abc0
NasWin11 <1270071138@qq.com>
2024-12-28 03:03:42 +0800

    game

--- game_alone/ScreenOverlay.py
+++ game_alone/ScreenOverlay.py
@@ -1,7 +1,9 @@
+import threading
 import tkinter as tk
 
+
 class TransparentOverlay:
-    def __init__(self,width,height):
+    def __init__(self, width, height):
         self.root = tk.Tk()
         self.root.attributes("-fullscreen", True)  # 全屏
         self.root.attributes("-transparentcolor", "black")  # 设置透明色
@@ -30,19 +32,22 @@ class TransparentOverlay:
             self.rect_x1, self.rect_y1, self.rect_x2, self.rect_y2, outline="red", width=5
         )
 
-
         # 点击穿透功能
         self.root.attributes("-transparentcolor", "black")
 
     def open_overlay(self):
         """打开窗口"""
-        self.root.deiconify()
-        self.root.after(0, self.root.mainloop)
+        threading.Thread(target=self._run_overlay, daemon=True).start()
+
+    def _run_overlay(self):
+        self.root.mainloop()
 
     def close_overlay(self):
         """关闭窗口"""
         self.root.destroy()
 
+
 if __name__ == "__main__":
-    overlay = TransparentOverlay()
+    overlay = TransparentOverlay(100,200)
     overlay.open_overlay()
+    print("Overlay is running in a non-blocking thread.")
